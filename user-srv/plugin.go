93596a6f5b99c165d18e81c229c8ff9d15676289
wenyxu <wenymedia@gmail.com>
2019-12-30 21:24:50 +0800

    fix(user-srv/plugin.go):fix bugs

--- user-srv/plugin.go
+++ user-srv/plugin.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-_ "github.com/go-sql-driver/mysql"
-_ "github.com/micro-in-cn/tutorials/microservice-in-micro/part4/plugins/db"
-_ "github.com/micro-in-cn/tutorials/microservice-in-micro/part4/plugins/redis"
+	_ "github.com/go-sql-driver/mysql"
+	_ "go-mini-kit.com/plugins/db"
+	_ "go-mini-kit.com/plugins/redis"
 )
